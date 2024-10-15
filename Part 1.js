cec155e2b13f486c29392e27f269473257b260ae
Dwamenachrist <agyapongfrancis520@gmail.com>
2024-10-15 20:09:05 +0000

    Part 1: Basic Array Operations completed

---
+++ Part 1.js
@@ -0,0 +1,17 @@
+// Basic Array Operators
+let fruits = [ 'apple', 'banana', 'mango', 'orange', 'grape'] // Create a variable to store an array of fruits
+
+console.log(fruits[1]) // printing banana to the terminal
+console.log(fruits[3]) // printing orange to the terminal
+
+fruits[2] =  'strawberry'  // replacing mango with strawberry
+
+console.log(fruits) // this is to show the update in fruit array
+
+fruits.push('watermelon')
+
+console.log(fruits)
+
+fruits.shift()
+
+console.log(fruits)
