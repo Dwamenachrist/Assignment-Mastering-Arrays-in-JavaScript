e9d12df406fa9d22e93cb72ac889c8b7e571bfff
Dwamenachrist <agyapongfrancis520@gmail.com>
2024-10-16 19:54:21 +0000

    Part 3: Advanced Array Methods completed

--- Part 3.js
+++ Part 3.js
@@ -2,22 +2,22 @@
 
 let fruits = [ 'apple', 'banana', 'mango', 'orange', 'grape']
 
-let longFruits = fruits.filter(fruit => {
-    if (fruit.length > 5){
+let longFruits = fruits.filter(fruit => {       // The filter helps to create a new array that contains only the filtered fruits
+    if (fruit.length > 5){  // This is the filter condition to return only fruits with more than 5 characters
         return fruit;
     }
 })
-console.log(longFruits);
+console.log(longFruits);    // Print out the filtered fruits
 
-let sortFruits = fruits.sort()
-console.log(sortFruits)
+let sortFruits = fruits.sort()   // It sorts the fruits array alphabetically
+console.log(sortFruits)  // Print out the sorted array
 
 
-let reverseFruits = fruits.reverse()
-console.log(reverseFruits)
+let reverseFruits = fruits.reverse()        // It reverses the order of the fruits in the array
+console.log(reverseFruits)  // Print out the reverse fruits in the array
 
-let reduceFruitsArray = fruits.reduce( ( acc,fruit) => {
-    return acc + "," + fruit
+let reduceFruitsArray = fruits.reduce( ( acc,fruit) => {    // The reduce method put all the strings in the array together index by index
+    return acc + "," + fruit    // The fruits are returned and seperated by commas
 })
-console.log(reduceFruitsArray)
+console.log(reduceFruitsArray)      // Print out the accumulated fruit seperated by commas
 
