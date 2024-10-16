ebd2d8df39c375f0d315d6e68a2459e52f5813f6
Dwamenachrist <agyapongfrancis520@gmail.com>
2024-10-16 22:31:02 +0000

    Part 5: Challenges completed

--- Part 5.js
+++ Part 5.js
@@ -15,3 +15,10 @@ const RemoveDuplicates =(array) =>{     //  function to remove duplicates fruit 
 }
 
 RemoveDuplicates(mixedFruits) // Call the RemoveDuplicates function to remove duplicates from the mixedFruits array
+
+// Finding the Maximum Number in the array
+const numbers = [15, 24, 6, 89, 32]  // An array of numbers
+
+numbers.sort((b, a) => a - b)       // sort the array of numbers in descending order
+
+console.log(numbers[0])     //Print out the first number in the sorted array
